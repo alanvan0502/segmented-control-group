fc122d571ee61b1eaa74e1b51b700a2727bcaf9c
Alan Van <alancllvan@gmail.com>
2020-09-16 19:12:24 +1000

    more example update

--- app/src/main/java/com/alanvan/example/MainActivity.kt
+++ app/src/main/java/com/alanvan/example/MainActivity.kt
@@ -2,11 +2,23 @@ package com.alanvan.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.alanvan.segmented_control.SegmentedControlGroup
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var segmentedControlGroup: SegmentedControlGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        segmentedControlGroup = findViewById(R.id.segmented_control_group)
+
+        segmentedControlGroup.apply {
+            setOnSelectedOptionChangeCallback {
+                Toast.makeText(context, "Button ${it + 1} selected", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
